7bf9ca00eb7f09db65d2944b28df86e90d1c5443
TDEL-SyntheticBio <sandram6@usc.edu>
2024-05-06 14:06:57 -0700

    Add files via upload

---
+++ Bounded Morphospace Models/CBM1.1.0 - 14500/Simulation/ELUGM.py
@@ -0,0 +1,11 @@
+from cc3d import CompuCellSetup
+
+from ELUGMSteppables import ELUGMSteppable
+CompuCellSetup.register_steppable(steppable = ELUGMSteppable(_frequency = 1))
+
+from ELUGMSteppables import MitosisSteppable
+CompuCellSetup.register_steppable(steppable = MitosisSteppable(_frequency = 1))
+
+CompuCellSetup.run()
+        
+        
